0ae21a90dd4219ec3d6a326ea8c1303edbd9d374
metime00 <metime00@gmail.com>
2015-04-21 00:25:06 -0400

    added window for eventual gui

---
+++ TheoryBlockPuzzle/Window.fs
@@ -0,0 +1,33 @@
+﻿module Window
+
+open System
+open System.Collections.Generic
+open System.Drawing
+open System.Windows.Forms
+
+type Window () as this =
+    inherit Form ()
+
+    let endGame args =
+        
+        ()
+
+    do
+        this.Text <- "John Block Puzzle"
+        this.ClientSize <- new Size(640, 480)
+        this.SetStyle (ControlStyles.AllPaintingInWmPaint, true)
+        this.SetStyle (ControlStyles.UserPaint, true)
+        this.SetStyle (ControlStyles.OptimizedDoubleBuffer, true)
+        this.FormBorderStyle <- FormBorderStyle.Fixed3D
+        this.MaximizeBox <- false
+        this.BackColor <- Color.Tan
+        
+
+    override this.OnMouseMove args =
+        ()
+    
+    override this.OnMouseDown args =
+        ()      
+
+    override this.OnPaint args =
+        ()
